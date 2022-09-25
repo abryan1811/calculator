94022258d0dfe57fb9c67ab4a95f769443db6214
abryan1811 <adam181189@gmail.com>
2022-09-25 14:53:40 +0100

    added if else statements for operator

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ console.log("Welcome to the Calculator!");
 console.log("=============================");
 
 
+console.log("Please type in your operator (add, subtract, divide or multiply):")
+const operator = readline.prompt();
+
 console.log("Choose your first number");
-const number1 = readline.prompt();
+const number1 = +readline.prompt();
 
 console.log("Choose your second number");
-const number2 = readline.prompt();
+const number2 = +readline.prompt();
 
-const total = number1 * number2;
+let total = 0
+if (operator == "add") {
+    total = number1 + number2;
+} else if (operator == "subtract") {
+    total = number1 - number2;
+} else if (operator == "divide") {
+    total = number1 / number2;
+} else if (operator == "multiply") {
+    total = number1 * number2;
+} else {
+    console.log("Not an option")
+    const x = readline.prompt();
+}
+
 console.log("Your number is " + total)
