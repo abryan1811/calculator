b932812f3b775146ebb786217fc81cb05a80735a
abryan1811 <adam181189@gmail.com>
2022-09-25 14:16:52 +0100

    Added a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Welcome to the Calculator!")
+const readline = require('readline-sync');
+
+console.log("Welcome to the Calculator!");
+console.log("=============================");
+
+
+console.log("Choose your first number");
+const number1 = readline.prompt();
+
+console.log("Choose your second number");
+const number2 = readline.prompt();
+
+const total = number1 * number2;
+console.log("Your number is " + total)
